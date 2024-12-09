a34914d60d4cb72e964db0d4a04727c2d5eda20c
Krishnan Singaram <ankrishnadev@gmail.com>
2024-12-09 23:55:04 +0530

    Added: Popular TV shows

--- tv/src/main/java/com/krishna/androidtvapp/MainActivity.kt
+++ tv/src/main/java/com/krishna/androidtvapp/MainActivity.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.tv.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Surface
+import androidx.tv.material3.Text
 import com.krishna.androidtvapp.ui.components.NavigationDrawerUI
 import com.krishna.androidtvapp.ui.theme.AndroidTVAppTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalTvMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
